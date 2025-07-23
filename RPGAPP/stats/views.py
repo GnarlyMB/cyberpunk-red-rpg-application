aa20271aac0f11d2e1a9531cfbc17e5ade2e0ac3
GnarlyMB <bodhibouttamy@gmail.com>
2025-07-23 18:24:51 -0400

    :)

--- RPGAPP/stats/views.py
+++ RPGAPP/stats/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-#def stats(request):
-#    return render(request, 'stats.html')
+def stats(request):
+    return render(request, 'stats.html')
 
-class stats_view(View):
-    def stats(self, request):
-        stats = NpcStatistics.objects.all()
-        return render(request, 'stats.html', context={'stats':stats})
+#class stats_view(View):
+#    def stats(self, request):
+#        stats = NpcStatistics.objects.all()
+#        return render(request, 'stats.html', context={'stats':stats})
+
